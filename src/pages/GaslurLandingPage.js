a77be0d13f57a1d9c3c57f32906ab42a1a2c10d4
Noor Illahi <noorillahi@fastechsystem.co>
2023-11-14 12:53:36 +0500

    second commit

---
+++ src/pages/GaslurLandingPage.js
@@ -0,0 +1,444 @@
+import styles from "./GaslurLandingPage.module.css";
+
+const GaslurLandingPage = () => {
+  return (
+    <div className={styles.gaslurLandingPage}>
+      <div className={styles.gaslurLandingPageChild} />
+      <div className={styles.gaslurLandingPageItem} />
+      <div className={styles.gaslurLandingPageInner} />
+      <div className={styles.discoverCollectAnd}>
+        Discover, collect, and charity in extraordinary NFT marketplace
+      </div>
+      <div className={styles.inAeneanPosuere}>
+        In aenean posuere lorem risus nec. Tempor tincidunt aenean purus purus
+        vestibulum nibh mi venenatis
+      </div>
+      <div className={styles.exploreWrapper}>
+        <div className={styles.explore}>Explore</div>
+      </div>
+      <div className={styles.createWrapper}>
+        <div className={styles.explore}>Create</div>
+      </div>
+      <div className={styles.signInParent}>
+        <div className={styles.signIn}>sign in</div>
+        <div className={styles.createParent}>
+          <div className={styles.create1}>create</div>
+          <div className={styles.groupChild} />
+        </div>
+      </div>
+      <div className={styles.loadMoreParent}>
+        <div className={styles.loadMore}>Load more</div>
+        <div className={styles.groupItem} />
+      </div>
+      <img className={styles.heart24Icon} alt="" src="/heart24.svg" />
+      <div className={styles.laura}>Laura</div>
+      <div className={styles.weAreHere}>WE ARE HERE</div>
+      <div className={styles.weth}>0.21 Weth</div>
+      <div className={styles.loremIpsumParent}>
+        <div className={styles.loremIpsum}>Lorem Ipsum</div>
+        <div className={styles.weth1}>1.20 Weth</div>
+      </div>
+      <div className={styles.loremIpsumGroup}>
+        <div className={styles.loremIpsum}>Lorem Ipsum</div>
+        <div className={styles.weth1}>1.20 Weth</div>
+      </div>
+      <div className={styles.loremIpsumContainer}>
+        <div className={styles.loremIpsum}>Lorem Ipsum</div>
+        <div className={styles.weth1}>1.20 Weth</div>
+      </div>
+      <div className={styles.dolorSitAmetParent}>
+        <div className={styles.loremIpsum}>Dolor sit amet</div>
+        <div className={styles.weth1}>0.21 Weth</div>
+      </div>
+      <div className={styles.dolorSitAmetGroup}>
+        <div className={styles.loremIpsum}>Dolor sit amet</div>
+        <div className={styles.weth1}>0.21 Weth</div>
+      </div>
+      <div className={styles.dolorSitAmetContainer}>
+        <div className={styles.loremIpsum}>Dolor sit amet</div>
+        <div className={styles.weth1}>0.21 Weth</div>
+      </div>
+      <div className={styles.dignissimCurabituParent}>
+        <div className={styles.loremIpsum}>Dignissim curabitu..</div>
+        <div className={styles.weth1}>0.01 Weth</div>
+      </div>
+      <div className={styles.dignissimCurabituGroup}>
+        <div className={styles.loremIpsum}>Dignissim curabitu..</div>
+        <div className={styles.weth1}>0.01 Weth</div>
+      </div>
+      <div className={styles.dignissimCurabituContainer}>
+        <div className={styles.loremIpsum}>Dignissim curabitu..</div>
+        <div className={styles.weth1}>0.01 Weth</div>
+      </div>
+      <div className={styles.ametPellentesqParent}>
+        <div className={styles.loremIpsum}>Amet pellentesq..</div>
+        <div className={styles.weth1}>0.55 Weth</div>
+      </div>
+      <div className={styles.ametPellentesqGroup}>
+        <div className={styles.loremIpsum}>Amet pellentesq..</div>
+        <div className={styles.weth1}>0.55 Weth</div>
+      </div>
+      <div className={styles.ametPellentesqContainer}>
+        <div className={styles.loremIpsum}>Amet pellentesq..</div>
+        <div className={styles.weth1}>0.55 Weth</div>
+      </div>
+      <img className={styles.groupIcon} alt="" src="/group-1248.svg" />
+      <div className={styles.div}>25</div>
+      <div className={styles.hotAuctions}>Hot auctions</div>
+      <img
+        className={styles.unsplashrujm3dbxcqwIcon}
+        alt=""
+        src="/unsplashrujm3dbxcqw@2x.png"
+      />
+      <img
+        className={styles.unsplashlki74jj7hUIcon}
+        alt=""
+        src="/unsplashlki74jj7hu@2x.png"
+      />
+      <img
+        className={styles.unsplashtryl7jyath0Icon}
+        alt=""
+        src="/unsplashtryl7jyath0@2x.png"
+      />
+      <img
+        className={styles.unsplash9xngoipxceoIcon}
+        alt=""
+        src="/unsplash9xngoipxceo@2x.png"
+      />
+      <img
+        className={styles.unsplashud5drkd4h6wIcon}
+        alt=""
+        src="/unsplashud5drkd4h6w@2x.png"
+      />
+      <img
+        className={styles.unsplashfg7j6nnebbcIcon}
+        alt=""
+        src="/unsplashfg7j6nnebbc@2x.png"
+      />
+      <img
+        className={styles.unsplashwn9du73b8SIcon}
+        alt=""
+        src="/unsplashwn9du73b8-s@2x.png"
+      />
+      <img
+        className={styles.unsplashhos3dzgphzwIcon}
+        alt=""
+        src="/unsplashhos3dzgphzw@2x.png"
+      />
+      <img
+        className={styles.unsplashbu8texhspcyIcon}
+        alt=""
+        src="/unsplashbu8texhspcy@2x.png"
+      />
+      <img
+        className={styles.unsplash7pqszt9kieyIcon}
+        alt=""
+        src="/unsplash7pqszt9kiey@2x.png"
+      />
+      <img
+        className={styles.unsplashcrs2vlkse98Icon}
+        alt=""
+        src="/unsplashcrs2vlkse98@2x.png"
+      />
+      <img
+        className={styles.unsplash9adeedyb2ykIcon}
+        alt=""
+        src="/unsplash9adeedyb2yk@2x.png"
+      />
+      <div className={styles.viewAllParent}>
+        <div className={styles.viewAll}>View all</div>
+        <img
+          className={styles.arrowRight24Icon}
+          alt=""
+          src="/arrowright24.svg"
+        />
+      </div>
+      <div className={styles.homeParent}>
+        <b className={styles.home}>Home</b>
+        <div className={styles.myProfile}>My profile</div>
+        <div className={styles.activity}>activity</div>
+        <div className={styles.howItWorks}>how it works</div>
+        <div className={styles.groupInner} />
+      </div>
+      <img className={styles.lineIcon} alt="" src="/line-11.svg" />
+      <img
+        className={styles.gaslurLandingPageChild1}
+        alt=""
+        src="/line-11.svg"
+      />
+      <img
+        className={styles.gaslurLandingPageChild2}
+        alt=""
+        src="/line-11.svg"
+      />
+      <img
+        className={styles.gaslurLandingPageChild3}
+        alt=""
+        src="/line-11.svg"
+      />
+      <img
+        className={styles.gaslurLandingPageChild4}
+        alt=""
+        src="/line-11.svg"
+      />
+      <img
+        className={styles.gaslurLandingPageChild5}
+        alt=""
+        src="/line-11.svg"
+      />
+      <img
+        className={styles.gaslurLandingPageChild6}
+        alt=""
+        src="/line-11.svg"
+      />
+      <img
+        className={styles.gaslurLandingPageChild7}
+        alt=""
+        src="/line-11.svg"
+      />
+      <img
+        className={styles.gaslurLandingPageChild8}
+        alt=""
+        src="/line-11.svg"
+      />
+      <img
+        className={styles.gaslurLandingPageChild9}
+        alt=""
+        src="/line-11.svg"
+      />
+      <img
+        className={styles.gaslurLandingPageChild10}
+        alt=""
+        src="/line-11.svg"
+      />
+      <img
+        className={styles.gaslurLandingPageChild11}
+        alt=""
+        src="/line-11.svg"
+      />
+      <div className={styles.endsIn013445Parent}>
+        <div className={styles.endsIn013445Container}>
+          <span>Ends in</span>
+          <span className={styles.span}> 01.34.45</span>
+        </div>
+        <div className={styles.bidParent}>
+          <div className={styles.bid}>Bid</div>
+          <div className={styles.rectangleDiv} />
+        </div>
+      </div>
+      <div className={styles.endsIn013445Group}>
+        <div className={styles.endsIn013445Container}>
+          <span>Ends in</span>
+          <span className={styles.span}> 01.34.45</span>
+        </div>
+        <div className={styles.bidParent}>
+          <div className={styles.bid}>Buy</div>
+          <div className={styles.rectangleDiv} />
+        </div>
+      </div>
+      <div className={styles.groupDiv}>
+        <div className={styles.endsIn013445Container}>
+          <span>Ends in</span>
+          <span className={styles.span}> 01.34.45</span>
+        </div>
+        <div className={styles.bidParent}>
+          <div className={styles.bid}>Bid</div>
+          <div className={styles.rectangleDiv} />
+        </div>
+      </div>
+      <div className={styles.endsIn013445Parent1}>
+        <div className={styles.endsIn013445Container3}>
+          <span>Ends in</span>
+          <span className={styles.span}> 01.34.45</span>
+        </div>
+        <div className={styles.bidParent}>
+          <div className={styles.bid}>Bid</div>
+          <div className={styles.rectangleDiv} />
+        </div>
+      </div>
+      <div className={styles.endsIn013445Parent2}>
+        <div className={styles.endsIn013445Container3}>
+          <span>Ends in</span>
+          <span className={styles.span}> 01.34.45</span>
+        </div>
+        <div className={styles.bidParent}>
+          <div className={styles.bid}>Buy</div>
+          <div className={styles.rectangleDiv} />
+        </div>
+      </div>
+      <div className={styles.endsIn013445Parent3}>
+        <div className={styles.endsIn013445Container3}>
+          <span>Ends in</span>
+          <span className={styles.span}> 01.34.45</span>
+        </div>
+        <div className={styles.bidParent}>
+          <div className={styles.bid}>Bid</div>
+          <div className={styles.rectangleDiv} />
+        </div>
+      </div>
+      <div className={styles.endsIn564445Parent}>
+        <div className={styles.endsIn013445Container}>
+          <span>Ends in</span>
+          <span className={styles.span}> 56.44.45</span>
+        </div>
+        <div className={styles.bidParent}>
+          <div className={styles.bid}>Bid</div>
+          <div className={styles.rectangleDiv} />
+        </div>
+      </div>
+      <div className={styles.endsIn564445Group}>
+        <div className={styles.endsIn013445Container}>
+          <span>Ends in</span>
+          <span className={styles.span}> 56.44.45</span>
+        </div>
+        <div className={styles.bidParent}>
+          <div className={styles.bid}>Buy</div>
+          <div className={styles.rectangleDiv} />
+        </div>
+      </div>
+      <div className={styles.endsIn564445Parent1}>
+        <div className={styles.endsIn013445Container}>
+          <span>Ends in</span>
+          <span className={styles.span}> 56.44.45</span>
+        </div>
+        <div className={styles.bidParent}>
+          <div className={styles.bid}>Bid</div>
+          <div className={styles.rectangleDiv} />
+        </div>
+      </div>
+      <div className={styles.endsIn013445Parent4}>
+        <div className={styles.endsIn013445Container}>
+          <span>Ends in</span>
+          <span className={styles.span}> 01.34.45</span>
+        </div>
+        <div className={styles.bidParent4}>
+          <div className={styles.bid}>Bid</div>
+          <div className={styles.rectangleDiv} />
+        </div>
+      </div>
+      <div className={styles.endsIn013445Parent5}>
+        <div className={styles.endsIn013445Container}>
+          <span>Ends in</span>
+          <span className={styles.span}> 01.34.45</span>
+        </div>
+        <div className={styles.bidParent4}>
+          <div className={styles.bid}>Buy</div>
+          <div className={styles.rectangleDiv} />
+        </div>
+      </div>
+      <div className={styles.endsIn013445Parent6}>
+        <div className={styles.endsIn013445Container}>
+          <span>Ends in</span>
+          <span className={styles.span}> 01.34.45</span>
+        </div>
+        <div className={styles.bidParent4}>
+          <div className={styles.bid}>Bid</div>
+          <div className={styles.rectangleDiv} />
+        </div>
+      </div>
+      <div className={styles.groupParent}>
+        <div className={styles.groupContainer}>
+          <div className={styles.companyParent}>
+            <div className={styles.company}>Company</div>
+            <div className={styles.aboutUsParent}>
+              <div className={styles.aboutUs}>About Us</div>
+              <div className={styles.services}>Services</div>
+              <div className={styles.portfolio}>Portfolio</div>
+            </div>
+          </div>
+          <div className={styles.marketplaceParent}>
+            <div className={styles.marketplace}>Marketplace</div>
+            <div className={styles.homeGroup}>
+              <div className={styles.home1}>Home</div>
+              <div className={styles.activity1}>Activity</div>
+              <div className={styles.discover}>Discover</div>
+              <div className={styles.learnMore}>Learn more</div>
+            </div>
+          </div>
+          <div className={styles.contactParent}>
+            <div className={styles.contact}>Contact</div>
+            <div className={styles.facebookParent}>
+              <div className={styles.facebook}>Facebook</div>
+              <div className={styles.instagram}>Instagram</div>
+              <div className={styles.twitter}>Twitter</div>
+              <div className={styles.email}>Email</div>
+            </div>
+          </div>
+          <div className={styles.etiamEtId}>
+            Etiam et id tincidunt faucibus mollis a sociis pretium fermentum
+            quis magna faucibus lacus.
+          </div>
+        </div>
+        <div className={styles.copyright2021GaslurParent}>
+          <div className={styles.copyright2021Gaslur}>
+            Copyright 2021 Gaslur
+          </div>
+          <div className={styles.lineDiv} />
+          <div className={styles.instagramParent}>
+            <img
+              className={styles.instagramIcon}
+              alt=""
+              src="/instagram@2x.png"
+            />
+            <img
+              className={styles.facebookIcon}
+              alt=""
+              src="/facebook@2x.png"
+            />
+            <img
+              className={styles.twitterSquaredIcon}
+              alt=""
+              src="/twitter-squared@2x.png"
+            />
+          </div>
+        </div>
+      </div>
+      <div className={styles.discoverParent}>
+        <div className={styles.discover1}>Discover</div>
+        <div className={styles.rectangleParent}>
+          <div className={styles.groupChild12} />
+          <div className={styles.filterParent}>
+            <div className={styles.filter}>Filter</div>
+            <img
+              className={styles.iconoutlinefilter}
+              alt=""
+              src="/iconoutlinefilter.svg"
+            />
+          </div>
+        </div>
+        <div className={styles.cheapestParent}>
+          <div className={styles.cheapest}>Cheapest</div>
+          <img
+            className={styles.iconoutlinecheveronDown}
+            alt=""
+            src="/iconoutlinecheverondown.svg"
+          />
+        </div>
+        <div className={styles.newestParent}>
+          <div className={styles.newest}>Newest</div>
+          <img
+            className={styles.iconoutlinecheveronDown1}
+            alt=""
+            src="/iconoutlinecheverondown1.svg"
+          />
+        </div>
+        <div className={styles.categoryParent}>
+          <div className={styles.category}>Category</div>
+          <img
+            className={styles.iconoutlinesortAscending}
+            alt=""
+            src="/iconoutlinesortascending.svg"
+          />
+        </div>
+      </div>
+      <img
+        className={styles.unsplashog44d93injkIcon}
+        alt=""
+        src="/unsplashog44d93injk@2x.png"
+      />
+      <b className={styles.fastech}>FasTech</b>
+    </div>
+  );
+};
+
+export default GaslurLandingPage;
